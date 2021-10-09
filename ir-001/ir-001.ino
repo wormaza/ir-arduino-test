860988e54cf05129d668e3266d63fd33d9c427d0
wormaza <wladimir.ormazabal@gmail.com>
2021-10-08 22:39:06 -0300

    ordenando primera prueba

--- ir-001/ir-001.ino
+++ ir-001/ir-001.ino
@@ -1,9 +1,21 @@
-int pd=2;                      //Photodiode to digital pin 2
-float sinVal;  
-int toneVal;
- int buzz=12;                   //piezo buzzer to digital pin 13  
+ int pd=2;                      //Photodiode to digital pin 2 
+   
  int senRead=0;                 //Readings from sensor to analog pin 0  
- int limit=700;                 //Threshold range of an obstacle  
+ int limit=600;                 //Threshold range of an obstacle  
+
+ int buzz=12;                   //piezo buzzer to digital pin 12
+ float sinVal;  
+ int toneVal;
+ void buzzAlarm()               //CONTROLA BUZZ COMO ALARMA
+ {
+    for(int x=0; x<180; x++){
+            sinVal = (sin(x*(3.1412/180)));
+            toneVal = 2000+(int(sinVal*1000));
+            tone(buzz, toneVal);
+            delay(2); 
+    } 
+ }
+ 
  void setup()    
  {  
   pinMode(pd,OUTPUT);  
@@ -13,25 +25,22 @@ int toneVal;
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);          //setting serial monitor at a default baund rate of 9600  
  }  
+
+ 
  void loop()  
  {  
   int val=analogRead(senRead);  //variable to store values from the photodiode  
   Serial.println(val);          // prints the values from the sensor in serial monitor  
   if(val <= limit)              //If obstacle is nearer than the Threshold range  
   {  
-   for(int x=0; x<180; x++){
-            sinVal = (sin(x*(3.1412/180)));
-            toneVal = 2000+(int(sinVal*1000));
-            tone(buzz, toneVal);
-            delay(2); 
-          } 
    digitalWrite(LED_BUILTIN, HIGH);
-   delay(1000);
+   buzzAlarm();
+   delay(500);
   }  
   else if(val > limit)          //If obstacle is not in Threshold range  
   {  
    noTone(buzz);
    digitalWrite(LED_BUILTIN, LOW);
-   delay(1000); 
+   delay(500); 
   }  
  }  
