1009aad7a1dba1efdce1bfbe14ac9cd4ef364753
wormaza <wladimir.ormazabal@gmail.com>
2021-10-20 21:11:16 -0300

    ejemplo 2 solo con led 2

--- ir-002/ir-002.ino
+++ ir-002/ir-002.ino
@@ -1,13 +1,18 @@
+ #include <Servo.h>
+ 
  int photodiodeDigital=2;              //digital pin 2 
  int photodiodeAnalog=0;               //analog pin 0  
  int limit=600;                        //Threshold range of an obstacle  
 
+ Servo servoMotor;
  
  void setup()    
  {  
   pinMode(photodiodeDigital,OUTPUT);  
   digitalWrite(photodiodeDigital,HIGH);       //supply 5 volts to photodiode  
   pinMode(LED_BUILTIN, OUTPUT);
+  servoMotor.attach(9);
+  servoMotor.write(0);
   Serial.begin(9600);          //setting serial monitor at a default baund rate of 9600  
  }  
 
@@ -19,11 +24,13 @@
   if(val <= limit)              //If obstacle is nearer than the Threshold range  
   {  
    digitalWrite(LED_BUILTIN, HIGH);
+   servoMotor.write(90);
    delay(500);
   }  
   else if(val > limit)          //If obstacle is not in Threshold range  
   {  
    digitalWrite(LED_BUILTIN, LOW);
+    servoMotor.write(0);
    delay(500); 
   }  
  }  
